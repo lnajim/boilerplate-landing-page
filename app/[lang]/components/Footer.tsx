7c80cc32670c786296a1f037abeeb16f80397adb
Lotfi Najim <lotfi.najim@outlook.com>
2024-09-24 13:13:37 +0400

    impmenet translation store

--- app/[lang]/components/Footer.tsx
+++ app/[lang]/components/Footer.tsx
@@ -1,16 +1,18 @@
 // components/Footer.tsx
 
+import useTranslationStore from "@/stores/TranslationStore"
+
 interface FooterProps {
-	dictionary: {
-		copyright: string;
-	}
+
 }
 
-const Footer: React.FC<FooterProps> = ({ dictionary }) => {
+const Footer: React.FC<FooterProps> = () => {
+	const { dictionary } = useTranslationStore()
+
 	return (
 		<footer className="bg-purple-600 text-white py-8">
 			<div className="container mx-auto text-center">
-				<p>{dictionary.copyright.replace('{year}', new Date().getFullYear().toString())}</p>
+				<p>{dictionary.Footer.copyright.replace('{year}', new Date().getFullYear().toString())}</p>
 			</div>
 		</footer>
 	)
