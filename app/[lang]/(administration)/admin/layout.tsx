e7e2224a94b0306814d5920d6e57c33d3232d861
Lotfi Najim <lotfi.najim@outlook.com>
2024-09-30 08:43:53 +0400

    udpate routes.ts

--- app/[lang]/(administration)/admin/layout.tsx
+++ app/[lang]/(administration)/admin/layout.tsx
@@ -4,9 +4,7 @@ import TanstackProvider from '@/providers/tanstack-provider';
 export default async function AdminLayout({ children }: { children: React.ReactNode }) {
 	const session = await auth();
 	return (
-
 		<TanstackProvider>
-
 			<div className="flex-grow mt-16 p-5 overflow-y-auto bg-slate-100 md:mt-24">
 				<main>{children}</main>
 			</div>
