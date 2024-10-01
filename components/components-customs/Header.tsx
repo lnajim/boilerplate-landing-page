9ef450d56ea47a011938e74309f016dd7f9428fc
Lotfi Najim <lotfi.najim@outlook.com>
2024-10-01 15:02:03 +0400

    logo header

--- components/components-customs/Header.tsx
+++ components/components-customs/Header.tsx
@@ -1,14 +1,15 @@
 'use client'
 
 import Link from 'next/link'
+import Image from 'next/image'
 import { useEffect, useState } from 'react'
 import useTranslationStore from '@/stores/TranslationStore'
-import { headerConfig } from '@/app/config/headerConfig'
 import { appConfig } from '@/app.config'
 import MobileMenu from './MobileMenu'
 import { Menu, X } from 'lucide-react'
 import { Button } from "@/components/ui/button"
 import DesktopMenu from './DesktopMenu'
+import { headerConfig } from '@/app/config/headerConfig'
 
 const Header: React.FC = () => {
 	const { language, dictionary, setLanguage } = useTranslationStore()
@@ -30,7 +31,8 @@ const Header: React.FC = () => {
 	return (
 		<header className="bg-gradient-to-r from-purple-500 to-pink-500 p-4" data-header-id="main-header">
 			<div className="container mx-auto flex justify-between items-center">
-				<Link href="/" className="text-white text-2xl font-cursive">
+				<Link href="/" className="flex items-center text-white text-2xl font-cursive">
+					<Image src={appConfig.header.logo} alt="Logo" width={240} height={61} className='mr-2' />
 					{appConfig.header.applicationName}
 				</Link>
 				<DesktopMenu
