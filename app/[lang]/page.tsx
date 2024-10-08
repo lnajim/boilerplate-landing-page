0dd31173da63e6a9c9f723474ddd14fe27b054f1
Lotfi Najim <lotfi.najim@outlook.com>
2024-10-08 10:32:41 +0400

    update layout

--- app/[lang]/page.tsx
+++ app/[lang]/page.tsx
@@ -1,8 +1,11 @@
 'use client'
 
 import { ContactForm, Footer } from "@/components/components-customs"
+import TwoColumnContactForm from "@/components/components-customs/contact-form/TwoColumnContactForm"
 import Header from "@/components/components-customs/headers/Header"
+import TypingAnimationHero from "@/components/components-customs/hero-sections/TypingAnimationHero"
 import HeroSection from "@/components/components-customs/HeroSection"
+import CarouselServiceSection from "@/components/components-customs/service-sections/CarouselServiceSection"
 import ServiceSection from "@/components/components-customs/ServiceSection"
 import useTranslationStore from "@/stores/TranslationStore"
 
@@ -12,9 +15,9 @@ export default function Home() {
   return (
     <div>
       <Header />
-      <HeroSection />
-      <ServiceSection />
-      <ContactForm />
+      <TypingAnimationHero />
+      <CarouselServiceSection />
+      <TwoColumnContactForm />
       <Footer />
     </div>
   )
