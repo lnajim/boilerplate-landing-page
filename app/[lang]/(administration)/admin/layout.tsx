5db4bb422ae0aba0e986866eba44f071a76abe7f
Lotfi Najim <lotfi.najim@outlook.com>
2024-10-09 11:13:17 +0400

    bottom nav bar done

--- app/[lang]/(administration)/admin/layout.tsx
+++ app/[lang]/(administration)/admin/layout.tsx
@@ -4,6 +4,7 @@ import { useState, useEffect } from 'react'
 import AdminSidebar from '@/components/components-customs/AdminSidebar'
 import AdminHeader from '@/components/components-customs/AdminHeader'
 import TanstackProvider from '@/providers/tanstack-provider'
+import AdminBottomNav from '@/components/components-customs/AdminBottomNav'
 
 export default function AdminLayout({ children }: { children: React.ReactNode }) {
 	const [sidebarExpanded, setSidebarExpanded] = useState(false)
@@ -35,7 +36,7 @@ export default function AdminLayout({ children }: { children: React.ReactNode })
 			<div className="flex flex-col h-screen bg-slate-100">
 				<AdminHeader toggleSidebar={toggleSidebar} isExpanded={sidebarExpanded} />
 				<div className="flex flex-1 pt-16">
-					<AdminSidebar isExpanded={sidebarExpanded} />
+					{!isMobile && <AdminSidebar isExpanded={sidebarExpanded} />}
 					<div
 						className={`
 							flex-grow 
@@ -43,12 +44,14 @@ export default function AdminLayout({ children }: { children: React.ReactNode })
 							transition-all 
 							duration-300 
 							ease-in-out
-							${sidebarExpanded && !isMobile ? 'ml-64' : 'ml-16'}
+							${isMobile ? 'ml-0' : (sidebarExpanded ? 'ml-64' : 'ml-16')}
+							${isMobile ? 'mb-16' : 'mb-0'} // Add bottom margin for mobile
 						`}
 					>
 						<main className="p-5">{children}</main>
 					</div>
 				</div>
+				{isMobile && <AdminBottomNav />}
 			</div>
 		</TanstackProvider>
 	)
